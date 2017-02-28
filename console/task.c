5ee9c82d4e6958ef7d9322020148ef5f761045d7
Nikita Malyavin <nikitamalyavin@gmail.com>
2017-03-01 03:19:25 +1000

    implement correct cyrillic unicode handling

--- console/task.c
+++ console/task.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+﻿#include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 #include <math.h>
@@ -10,12 +10,12 @@ extern char _binary_students_txt_end;
 
 const char *tasks[] = {"tail", "head", "wc", "echo", "cat", "cp", "mv", "ar", "sort", "date", "du", "ls",};
 
-int find(char *s, char *end)
+static int find(char *s, char *end)
 {
     char *pos = strstr(_binary_students_txt_start, s);
 
     int num = -1;
-    for(char *newline = _binary_students_txt_start; newline != NULL && newline < pos; newline = strchr(newline + 1, '\n'))
+    for (char *newline = _binary_students_txt_start; newline != NULL && newline <= pos; newline = strchr(newline + 1, '\n'))
     {
         num++;
         volatile int r = rand();
@@ -23,7 +23,7 @@ int find(char *s, char *end)
     return num;
 }
 
-int strrev(char *s, char *end)
+static int strrev(char *s, char *end)
 {
     char c;
     while (s < end - 1)
@@ -38,12 +38,15 @@ int strrev(char *s, char *end)
     }
 }
 
-void flip_name(char *name, char *surname, char *end)
+static void flip_name(char *name, char *surname, char *end)
 {
     strrev(name, surname - 1);
     strrev(surname, end);
     strrev(name, end);
 }
+
+void utf8_to_cp1251(char*);
+
 
 int main()
 {
@@ -51,7 +54,7 @@ int main()
 
     srand(81771);
 
-    char name[100];
+    char wname[100], name[100];
     char fmt[] = "%99s";
 
     puts("Введите имя и фамилию");
@@ -63,18 +66,26 @@ int main()
 
     name[sz] = ' ';
 
+    scanf(fmt, name + sz + 1);
+
     int name_sz = sz;
 
-    scanf(fmt, name + sz + 1);
-    
     sz = strlen(name + sz + 1);
+    
+    strcpy(wname, name);
 
-    char *end = name + name_sz + 1 + sz + 1;
+    utf8_to_cp1251(name);
+    
+
+    char *wend = wname + name_sz + 1 + sz + 1;
+    char *end  = name + name_sz / 2 + sz / 2 + 2;
 
     int pos = find(name, end);
     if (pos == -1)
     {
-        flip_name(name, name + name_sz + 1, end);
+        flip_name(name, name + name_sz / 2 + 1, end - 1);
+        flip_name(wname, wname + name_sz + 1, wend - 1);
+
         pos = find(name, end);
         name_sz = sz;
     }
@@ -82,13 +93,14 @@ int main()
     if (pos == -1)
     {
         puts("U kidin mi!");
+        return 0;
     }
-    printf("pos %d\n", pos);
+    
     int task_no = rand() % ARRSZ(tasks);
-    printf("%d\n", task_no);
+
     printf("%s, Ваше задание -- реализовать консольную утилиту %s "
-               "(см. https://www.opennet.ru/man.shtml?topic=%s&category=1 )\n",
-           name + name_sz + 1,
+               "(см. https://www.opennet.ru/man.shtml?topic=%s&category=1)\n",
+           wname + name_sz + 1,
            tasks[task_no],
            tasks[task_no]);
 
