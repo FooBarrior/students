0abf4e16f1aefebe12ff93e2e3e2fe05143c173c
Nikita Malyavin <nikitamalyavin@gmail.com>
2018-04-06 02:00:49 +1000

    Fix Russian "yo" letter issue

--- console/task.c
+++ console/task.c
@@ -45,12 +45,22 @@ static void flip_name(char *name, char *surname, char *end)
     strrev(name, end);
 }
 
+static void ye_to_yo(char *s)
+{
+    do
+    {
+        *s += *s == (char)0xb8 || *s == (char)0xa8 ? 0xe5 - 0xb8 : 0;
+    } while (*s++);
+}
+
 void utf8_to_cp1251(char*);
 
 
 int main()
 {
     _binary_students_txt_end = 0;
+
+    ye_to_yo(_binary_students_txt_start);
 
     srand(13372018);
 
